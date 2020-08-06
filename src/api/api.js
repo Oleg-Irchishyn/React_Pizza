f1c49838aa5379fd0249c14afffa85a35e98f0f6
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-08-06 09:35:23 +0300

    updating project

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,16 @@
 import *as axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://oleg-irchishyn.github.io/React_Pizza/./'
+  /*baseURL: 'https://oleg-irchishyn.github.io/React_Pizza/./'*/
+  baseURL: 'http://localhost:3001/'
 });
 
 
 export const mainAPI = {
   getPizzas: () => {
-    return instance.get('db.json').then(response => {
-      return response.data.pizzas;
+    return instance.get('pizzas').then(response => {
+      /*return response.data.pizzas;*/
+      return response.data;
     });
   }
 }
