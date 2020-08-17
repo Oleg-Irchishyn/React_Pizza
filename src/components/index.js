5d84cc2f075fe13c160df2d8acb9d0fe0e45aa12
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-08-17 17:05:16 +0300

    updating project

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Header } from './Header/Header';
 export { default as Categories } from './Categories/Categories';
 export { default as SortPopup } from './SortPopup/SortPopup';
 export { default as Cart } from './Cart/Cart';
+export { default as CartItem } from './Cart/CartItem/CartItem';
 export { default as MainContainer } from './Main/Main';
