80e7cb77bd355c661acb896a6e1739c34e4588bf
Oleg <oleg.irchishyn91@gmail.com>
2020-10-14 14:02:34 +0300

    added build folder

---
+++ build/precache-manifest.103a8033efa252ec7dd7f96e7c218b1c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04564d7bf7ed65786aa1cbb58800d6f3",
+    "url": "/React_Pizza/index.html"
+  },
+  {
+    "revision": "8d4140f1da06cf151b69",
+    "url": "/React_Pizza/static/css/2.312b1a89.chunk.css"
+  },
+  {
+    "revision": "c94a1496a983cb1a72b4",
+    "url": "/React_Pizza/static/css/main.08c886fc.chunk.css"
+  },
+  {
+    "revision": "8d4140f1da06cf151b69",
+    "url": "/React_Pizza/static/js/2.e2f5da23.chunk.js"
+  },
+  {
+    "revision": "0cb50bd4c306edc174ecc490959eeccc",
+    "url": "/React_Pizza/static/js/2.e2f5da23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94a1496a983cb1a72b4",
+    "url": "/React_Pizza/static/js/main.66afdd21.chunk.js"
+  },
+  {
+    "revision": "842a2fddeac966aff104",
+    "url": "/React_Pizza/static/js/runtime-main.5955c7ae.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/React_Pizza/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/React_Pizza/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/React_Pizza/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/React_Pizza/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/React_Pizza/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/React_Pizza/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/React_Pizza/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/React_Pizza/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/React_Pizza/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/React_Pizza/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/React_Pizza/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/React_Pizza/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/React_Pizza/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React_Pizza/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React_Pizza/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React_Pizza/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React_Pizza/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React_Pizza/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/React_Pizza/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/React_Pizza/static/media/pizza-logo.f3327011.svg"
+  }
+]);
